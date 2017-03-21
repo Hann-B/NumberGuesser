392504dfcc93841b1416890c91ddbaa402f3dc15
hanna <hlbernard124@gmail.com>
2017-03-21 18:01:17 -0400

    level 1 again, I misread the last few steps

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,12 +14,14 @@ namespace ConsoleApp1
             int targetNumber = rnd.Next(100) + 1;
             int userGuess = 0;
             int guessCount = 0;
+            int[] pastGuessesArray = new int[5];
 
             Console.WriteLine("You have 5 guesses to get the right number!");
             Console.WriteLine("Guess a number between 1 and 100");
 
-            while (userGuess != targetNumber && guessCount <= 5)
+            while (userGuess != targetNumber && guessCount < 5)
             {
+                pastGuessesArray[guessCount] = userGuess;
                 int.TryParse(Console.ReadLine(), out userGuess);
 
                 if (userGuess < targetNumber)
@@ -32,10 +34,19 @@ namespace ConsoleApp1
                     guessCount++;
                     Console.WriteLine($"The number I am thinking of is lower than {userGuess}, Try again");
                 }
+                else if (guessCount == 4)
+                {
+                    //guessCount++;
+                    Console.WriteLine("You Lose, Whomp Whomp.");
+                }
                 else
                 {
                     Console.WriteLine("Great job! You guessed right!");
                 }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Your guesses this game have been {pastGuessesArray}");
             }
         }
     }
