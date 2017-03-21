49b2bba1ed8b7153136f264c2a2bf4005c9bef94
hanna <hlbernard124@gmail.com>
2017-03-21 16:04:54 -0400

    level 1 NumberGuesser

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int targetNumber = rnd.Next(100) + 1;
+            int userGuess = 0;
+            int guessCount = 0;
+
+            Console.WriteLine("You have 5 guesses to get the right number!");
+            Console.WriteLine("Guess a number between 1 and 100");
+
+            while (userGuess != targetNumber && guessCount <= 5)
+            {
+                int.TryParse(Console.ReadLine(), out userGuess);
+
+                if (userGuess < targetNumber)
+                {
+                    guessCount++;
+                    Console.WriteLine($"The number I am thinking of is higher than {userGuess}, Try again");
+                }
+                else if (userGuess > targetNumber)
+                {
+                    guessCount++;
+                    Console.WriteLine($"The number I am thinking of is lower than {userGuess}, Try again");
+                }
+                else
+                {
+                    Console.WriteLine("Great job! You guessed right!");
+                }
+            }
+        }
+    }
+}
+
